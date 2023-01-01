7ec0039225c4d30799e51456728cec5445533ac0
Dean Perry <deanperry@fastmail.com>
2023-01-01 21:21:48 +0000

    added pause, unpause, cancel, uncancel to subscriptions

--- lib/lemon_squeezy/resources/subscriptions.rb
+++ lib/lemon_squeezy/resources/subscriptions.rb
@@ -11,5 +11,82 @@ module LemonSqueezy
       Subscription.new(response.body["data"]) if response.success?
     end
 
+    # Kind: void or free
+    def pause(id:, kind:, resumes_at: nil)
+      body = {
+        data: {
+          type: "subscriptions",
+          id: id.to_s,
+          attributes: {
+            pause: {mode: kind, resumes_at: resumes_at}
+          }
+        }
+      }
+
+      response = patch_request("subscriptions/#{id}", body: body.to_json)
+      Subscription.new(response.body["data"]) if response.success?
+    end
+
+    def unpause(id:)
+      body = {
+        data: {
+          type: "subscriptions",
+          id: id.to_s,
+          attributes: {
+            pause: nil
+          }
+        }
+      }
+
+      response = patch_request("subscriptions/#{id}", body: body.to_json)
+      Subscription.new(response.body["data"]) if response.success?
+    end
+
+    def cancel(id:)
+      body = {
+        data: {
+          type: "subscriptions",
+          id: id.to_s,
+          attributes: {
+            cancelled: true
+          }
+        }
+      }
+
+      response = patch_request("subscriptions/#{id}", body: body.to_json)
+      Subscription.new(response.body["data"]) if response.success?
+    end
+
+    def uncancel(id:)
+      body = {
+        data: {
+          type: "subscriptions",
+          id: id.to_s,
+          attributes: {
+            cancelled: false
+          }
+        }
+      }
+
+      response = patch_request("subscriptions/#{id}", body: body.to_json)
+      Subscription.new(response.body["data"]) if response.success?
+    end
+
+    def change_plan(id:, plan_id:, variant_id:)
+      body = {
+        data: {
+          type: "subscriptions",
+          id: id.to_s,
+          attributes: {
+            product_id: plan_id,
+            variant_id: variant_id
+          }
+        }
+      }
+
+      response = patch_request("subscriptions/#{id}", body: body.to_json)
+      Subscription.new(response.body["data"]) if response.success?
+    end
+
   end
 end
